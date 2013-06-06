77bfbca29fda51620bd062fc1dab969346c4b5cc
normanbarber <normanebarber@gmail.com>
2013-06-06 12:02:02 -0400

    cleaning up unused code

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var app = require('http').createServer(handler)
-    , io = require('socket.io')
     , fs = require('fs')
-    , path = require('path')
     , port = Number(process.argv[2])
     , file = process.argv[3];
 
 app.listen(port);
 
 function handler (req, res) {
-    var filePath = req.url;
-    console.log('filePath = ' + filePath);
     fs.readFile(file,
         function (err, data) {
             if (err) {
