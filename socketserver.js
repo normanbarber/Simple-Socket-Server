11dc8ab850a4ffcdb8f3c17121ab9f3556586a24
normanbarber <normanebarber@gmail.com>
2013-06-10 14:08:36 -0400

    cleaning up one line not being used

--- socketserver.js
+++ socketserver.js
@@ -4,7 +4,6 @@ io.sockets.on('connection', function (socket) {
     socket.on('set_name', function(data){
         socket.set('username', data.name, function(){
             socket.emit('name_set', data);
-            socket.broadcast.send(JSON.stringify({type: 'newUser', message: 'Welcome to the simple chat server', name:data.name}));
         });
     });
 
