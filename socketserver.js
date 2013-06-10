6a68ba100cd476437aa1637a16efcf01a859bb83
normanbarber <normanebarber@gmail.com>
2013-06-10 11:10:42 -0400

    cleaning up notes and logs

--- socketserver.js
+++ socketserver.js
@@ -2,7 +2,7 @@ var io = require('socket.io').listen(3001);
 io.sockets.on('connection', function (socket) {
 
     socket.on('set_name', function(data){
-        socket.set('nickname', data.name, function(){
+        socket.set('username', data.name, function(){
             socket.emit('name_set', data);
             socket.broadcast.send(JSON.stringify({type: 'newUser', message: 'Welcome to the simple chat server', name:data.name}));
         });
@@ -10,8 +10,8 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('message', function(message){
         message = JSON.parse(message);
-        socket.get('nickname', function(err, nickname){
-            message.username = nickname;
+        socket.get('username', function(err, username){
+            message.username = username;
             socket.send(JSON.stringify(message));
             socket.broadcast.send(JSON.stringify(message));
         });
