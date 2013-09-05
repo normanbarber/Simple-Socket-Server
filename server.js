08319cf8a2f447fb03246bbb02898f17925d6744
normanbarber <normanebarber@gmail.com>
2013-09-05 13:07:40 -0400

    pushing http server and package.json

---
+++ server.js
@@ -0,0 +1,20 @@
+var app = require('http').createServer(handler)
+    , fs = require('fs')
+    , port = Number(process.argv[2])
+    , file = process.argv[3];
+
+app.listen(port);
+
+function handler (req, res) {
+    fs.readFile(file,
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading file');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
